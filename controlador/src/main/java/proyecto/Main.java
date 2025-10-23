d8f1d391d8dd3c426179bd90bd6d02c9c8c75cb7
SamiiHC-iespsur <samuel.de.cozar.al@iespoligonosur.org>
2025-10-24 00:53:48 +0200

    Vaciado de clase Main

--- controlador/src/main/java/proyecto/Main.java
+++ controlador/src/main/java/proyecto/Main.java
@@ -1,20 +1,7 @@
 package proyecto;
 
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Vista vista = new Vista();
-        Empresa empresa1 = vista.nuevaEmpresa();
-        vista.mostrarMensaje("Empresa creada:\n" + empresa1.toString());
-
-        Empresa empresa2 = vista.nuevaEmpresa();
-        vista.mostrarMensaje("Empresa creada:\n" + empresa2.toString());
-
-        Empresa empresa3 = vista.nuevaEmpresa();
-        vista.mostrarMensaje("Empresa creada:\n" + empresa3.toString());
-
-        List<Empresa> empresas = List.of(empresa1, empresa2, empresa3);
-        vista.imprimirEmpresas(empresas);
+        
     }
 }
