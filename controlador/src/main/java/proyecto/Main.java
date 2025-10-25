cefbd8f23606475113df28d9165890ea0509d219
Marcos-ArPo <marcos.arbin.pozo.al@iespoligonosur.org>
2025-10-25 17:03:40 +0200

    Clase Principal Completada

--- controlador/src/main/java/proyecto/Main.java
+++ controlador/src/main/java/proyecto/Main.java
@@ -1,7 +1,11 @@
 package proyecto;
 
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception{
+        Modelo mod = new Modelo();
+        Vista vis = new Vista();
+        Controlador ctrl = new Controlador(mod, vis);
+
+        ctrl.iniciar();
     }
 }
